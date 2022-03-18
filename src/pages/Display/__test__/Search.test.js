8ae783455fe6515e82c46498b1bff9e896c94a45
tundeph <54335828+tundeph@users.noreply.github.com>
2022-03-18 11:18:56 +0000

    Update Search.test.js

--- src/pages/Display/__test__/Search.test.js
+++ src/pages/Display/__test__/Search.test.js
@@ -1,6 +1,20 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import "@testing-library/jest-dom"
 import Search from "../Search"
+import { options } from "../Display"
 
 describe("Search Component Tests", () => {
-  it("should render checkbox on the page", () => {})
+  it("should render checkbox on the page", () => {
+    render(<Search options={options} handleSortBy={jest.fn} />)
+    const selectElement = screen.getAllByRole("checkbox")
+    expect(selectElement).toBeTruthy()
+  })
+
+  it("should click only one checkbox per time", () => {
+    render(<Search options={options} handleSortBy={jest.fn} />)
+    const nameSelectElement = screen.getByTestId("name")
+    const addedSelectElement = screen.getByTestId("added")
+    fireEvent.click(nameSelectElement)
+    expect(addedSelectElement).not.toBeChecked()
+  })
 })
