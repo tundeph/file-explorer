5ff25fea04c73f1a091f7d4217aae1d570aa72f9
tundeph <54335828+tundeph@users.noreply.github.com>
2023-01-14 19:10:46 +0000

    Update Display.tsx

--- src/pages/Display/Display.tsx
+++ src/pages/Display/Display.tsx
@@ -3,7 +3,7 @@ import React, { useState, useContext } from "react"
 import { ExplorerContext } from "../../context/ExplorerContext"
 
 //components
-import File from "../../components/File"
+import File from "@components/File"
 import Folder from "../../components/Folder"
 import Notification from "../../components/Notification"
 import Search from "./Search"
@@ -14,89 +14,89 @@ import { Container, SearchContainer, Title } from "../styles"
 
 //helper
 import {
-  nameAscending,
-  nameDescending,
-  dateAscending,
-  dateDescending,
-  flattenArray,
+	nameAscending,
+	nameDescending,
+	dateAscending,
+	dateDescending,
+	flattenArray,
 } from "../../helper/helper"
 
 export const options = {
-  name: {
-    title: "Name",
-    value: "name",
-    asc: nameAscending,
-    desc: nameDescending,
-    checked: false,
-  },
-  added: {
-    title: "Date",
-    value: "added",
-    asc: dateAscending,
-    desc: dateDescending,
-    checked: false,
-  },
+	name: {
+		title: "Name",
+		value: "name",
+		asc: nameAscending,
+		desc: nameDescending,
+		checked: false,
+	},
+	added: {
+		title: "Date",
+		value: "added",
+		asc: dateAscending,
+		desc: dateDescending,
+		checked: false,
+	},
 }
 
 const Display = () => {
-  const { data, isPending, error, dispatch } = useContext(ExplorerContext)
+	const { data, isPending, error, dispatch } = useContext(ExplorerContext)
 
-  //states
-  const [filterText, setFilterText] = useState<string>("")
+	//states
+	const [filterText, setFilterText] = useState<string>("")
 
-  //function to sort files
-  const handleSortBy = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const chosen = Object.values(options).map((option) => {
-      if (option.value === e.target.value) {
-        option.checked = e.target.checked
-        option.checked ? data?.sort(option.asc) : data?.sort(option.desc)
-        return option
-      } else {
-        option.checked = false
-        return option
-      }
-    })
+	//function to sort files
+	const handleSortBy = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const chosen = Object.values(options).map((option) => {
+			if (option.value === e.target.value) {
+				option.checked = e.target.checked
+				option.checked ? data?.sort(option.asc) : data?.sort(option.desc)
+				return option
+			} else {
+				option.checked = false
+				return option
+			}
+		})
 
-    dispatch({ type: "GET_DATA", payload: data })
-  }
+		dispatch({ type: "GET_DATA", payload: data })
+	}
 
-  //function to filter files with the input form
-  const filteredData = (data: any[], filterText: any) => {
-    if (filterText.trim().length > 0) {
-      return flattenArray(data).filter((d: { name: string }) =>
-        d.name.toLowerCase().includes(filterText.trim().toLowerCase())
-      )
-    } else {
-      return data
-    }
-  }
+	//function to filter files with the input form
+	const filteredData = (data: any[], filterText: any) => {
+		if (filterText.trim().length > 0) {
+			return flattenArray(data).filter((d: { name: string }) =>
+				d.name.toLowerCase().includes(filterText.trim().toLowerCase())
+			)
+		} else {
+			return data
+		}
+	}
 
-  return (
-    <Container>
-      <Title> File Explorer </Title>
-      {isPending && <Notification>Loading...</Notification>}
-      {error && <Notification>Error loading files/folders </Notification>}
+	return (
+		<Container>
+			<Title> File Explorer </Title>
+			{isPending && <Notification>Loading...</Notification>}
+			{error && <Notification>Error loading files/folders </Notification>}
 
-      {data && (
-        <>
-          <SearchContainer>
-            <Search options={options} handleSortBy={handleSortBy} />
-            <SearchBar filterText={filterText} setFilterText={setFilterText} />
-          </SearchContainer>
+			{data && (
+				<>
+					<SearchContainer>
+						<Search options={options} handleSortBy={handleSortBy} />
+						<SearchBar filterText={filterText} setFilterText={setFilterText} />
+					</SearchContainer>
 
-          {filteredData(data, filterText).map((eachData: any, i: number) => (
-            <React.Fragment key={i}>
-              {eachData.type === "folder" ? (
-                <Folder value={eachData} />
-              ) : (
-                <File value={eachData} />
-              )}
-            </React.Fragment>
-          ))}
-        </>
-      )}
-    </Container>
-  )
+					{filteredData(data, filterText).map((eachData: any, i: number) => (
+						<React.Fragment key={i}>
+							{eachData.type === "folder" ? (
+								<Folder value={eachData} />
+							) : (
+								<File value={eachData} />
+							)}
+						</React.Fragment>
+					))}
+				</>
+			)}
+		</Container>
+	)
 }
 
 export default Display
